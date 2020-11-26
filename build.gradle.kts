83e15c3f847dd61bec4112e9cac29976ac993a8b
pht2025 <pht2025@gmail.com>
2020-11-26 13:59:59 +0900

    * 스프링 버전 업그레이드.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.1.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
-    kotlin("plugin.jpa") version "1.3.72"
+    id("org.springframework.boot") version "2.4.0"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    kotlin("jvm") version "1.4.20"
+    kotlin("plugin.spring") version "1.4.20"
+    kotlin("plugin.jpa") version "1.4.20"
 }
 
 group = "com.pht"
 version = "0.0.6"
-java.sourceCompatibility = JavaVersion.VERSION_13
+java.sourceCompatibility = JavaVersion.VERSION_14
 
 configurations {
     compileOnly {
@@ -39,7 +39,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
@@ -48,13 +48,13 @@ dependencies {
     implementation("org.webjars.npm:bootstrap:4.5.0")
     implementation("org.webjars.npm:jquery:3.5.1")
     implementation("org.webjars.npm:popper.js:1.16.1-lts")
-    implementation("org.webjars.npm:moment:2.26.0")
-    implementation("org.webjars.npm:lodash:4.17.15")
+    implementation("org.webjars.npm:moment:2.29.1")
+    implementation("org.webjars.npm:lodash:4.17.20")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("org.assertj:assertj-core:3.16.1")
+    testImplementation("org.assertj:assertj-core:3.18.1")
     testImplementation("io.projectreactor:reactor-test")
 }
 
@@ -65,6 +65,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "13"
+        jvmTarget = "14"
     }
 }
